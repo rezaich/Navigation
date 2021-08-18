9967003e69ff0d3a8331755d83ee8dbe1d5666d6
rezaich <ichsanireza@gmail.com>
2021-08-18 14:01:46 +0700

    add deep link

--- app/src/main/java/com/zaich/navigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/zaich/navigation/DetailCategoryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.zaich.navigation.databinding.FragmentDetailCategoryBinding
 
 class DetailCategoryFragment : Fragment() {
@@ -35,6 +36,10 @@ class DetailCategoryFragment : Fragment() {
 
         binding.tvCategoryName.text = dataName
         binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnProfile.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
     }
 
     override fun onDestroy() {
